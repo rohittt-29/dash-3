5c24b8df712592b1ae76e34a844fa4714af65d15
rohittt-29 <rm2193352@gmail.com>
2025-04-21 14:49:14 +0530

    added logo

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export function SkeletonDemo() {
   return (
-    <div className="flex mt-20 ml-90 items-center space-x-4">
+    <div className="flex mt-20 ml md:ml-90 items-center space-x-4">
       <Skeleton className="h-12 w-12 rounded-full" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-[250px]" />
